f42c7502719fe748749bf2d80fa2f03ce0e844d6
Lohni <andi.lohninger@hotmail.com>
2019-07-11 15:49:46 +0200

    Entitiy

---
+++ HTL_Diplomprojekt-Backend/src/main/java/com/porscheinformatik/htl/entities/Vehicle.java
@@ -0,0 +1,106 @@
+package com.porscheinformatik.htl.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "VHC_ID")
+    private Long vhc_Id;
+    @Column(name = "BRAND")
+    private String brand;
+    @Column(name = "MODEL")
+    private String model;
+    @Column(name = "MILEAGE")
+    private String mileage;
+    @Column(name = "BUILD_YEAR")
+    private String buildyear;
+    @Column(name = "HP")
+    private Integer hp;
+    @Column(name = "CONDITION")
+    private String condition;
+    @Column(name = "FUELTYPE")
+    private String fueltype;
+    @Column(name = "COLOR")
+    private String color;
+
+    public Vehicle(String brand, String model){
+        this.brand=brand;
+        this.model=model;
+    }
+
+    public Long getVhc_Id() {
+        return vhc_Id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public String getBuildyear() {
+        return buildyear;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getFueltype() {
+        return fueltype;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public void setBuildyear(String buildyear) {
+        this.buildyear = buildyear;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public void setFueltype(String fueltype) {
+        this.fueltype = fueltype;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle: "+brand+";"+model+";"+mileage+";"+buildyear+";"+hp+";"+color;
+    }
+}
