12acd7f0c08c668c0dd67e2e4a2d65f9cd958e7b
Lohni <andi.lohninger@hotmail.com>
2019-07-16 11:42:32 +0200

    ICS-Generator

--- HTL_Diplomprojekt-Backend/src/main/java/com/porscheinformatik/htl/ICS_Generator.java
+++ HTL_Diplomprojekt-Backend/src/main/java/com/porscheinformatik/htl/ICS_Generator.java
@@ -9,24 +9,16 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class ICS_Generator {
-
-    private DateFormat dateformat = new SimpleDateFormat("yyyyMMdd");
-    private DateFormat timeformat = new SimpleDateFormat("HHmmss");
-
-    private String version =    "VERSION:2.0\r\n";
-    private String prodid =     "PRODID:server@projectname.com\r\n";
-    private String calBegin =   "BEGIN:VCALENDAR\r\n";
-    private String calEnd =     "END:VCALENDAR\r\n";
-    private String eventBegin = "BEGIN:VEVENT\r\n";
-    private String eventEnd =   "END:VEVENT\r\n";
 
     private String start_time, end_time, start_date, end_date;
     private String location, summary, description;
 
     public ICS_Generator(Termin termin){
+        DateFormat dateformat = new SimpleDateFormat("yyyyMMdd");
+        DateFormat timeformat = new SimpleDateFormat("HHmmss");
+
         this.start_date= dateformat.format(termin.getStart_date());
         this.start_time= timeformat.format(termin.getStart_time());
         this.end_date= dateformat.format(termin.getEnd_date());
@@ -55,30 +47,29 @@ public class ICS_Generator {
             String[] currdate = timeStamp.split("_");
             String uid = "UID:"+start_date+start_time+"@projectname.com\r\n";
 
-            StringBuilder builder = new StringBuilder();
-            builder.append(timeStamp);
-            builder.append(".ics");
-
-            File ics = new File(builder.toString());
+            String builder = timeStamp +
+                    ".ics";
+            File ics = new File(builder);
             if (!ics.exists()) {
                 ics.createNewFile();
             }
 
             FileWriter fw = new FileWriter(ics.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(calBegin);
-            bw.write(version);
-            bw.write(prodid);
+            bw.write("BEGIN:VCALENDAR\r\n");
+            bw.write("VERSION:2.0\r\n");
+            bw.write("PRODID:server@projectname.com\r\n");
+            String eventBegin = "BEGIN:VEVENT\r\n";
             bw.write(eventBegin);
             bw.write(uid);
-            if(location!="")bw.write("LOCATION:"+location+"\r\n");
-            if(summary!="")bw.write("SUMMARY:"+summary+"\r\n");
-            if(description!="")bw.write("DESCRIPTION:"+description+"\r\n");
+            if(!location.equals(""))bw.write("LOCATION:"+location+"\r\n");
+            if(!summary.equals(""))bw.write("SUMMARY:"+summary+"\r\n");
+            if(!description.equals(""))bw.write("DESCRIPTION:"+description+"\r\n");
             bw.write("DTSTART:"+start_date+"T"+start_time+"Z\r\n");
             bw.write("DTEND:"+end_date+"T"+end_time+"Z\r\n");
             bw.write("DTSTAMP:"+currdate[0]+"T"+currdate[1]+"Z\r\n");
-            bw.write(eventEnd);
-            bw.write(calEnd);
+            bw.write("END:VEVENT\r\n");
+            bw.write("END:VCALENDAR\r\n");
             bw.close();
         }
         catch (IOException e){
