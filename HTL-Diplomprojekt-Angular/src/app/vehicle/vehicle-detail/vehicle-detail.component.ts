10a715b8e1f38d415d90d7fdc0758bae1356fb4d
karnelu <luka.karner@outlook.com>
2019-07-24 15:54:20 +0200

    routing vehicle-detail

--- HTL-Diplomprojekt-Angular/src/app/vehicle/vehicle-detail/vehicle-detail.component.ts
+++ HTL-Diplomprojekt-Angular/src/app/vehicle/vehicle-detail/vehicle-detail.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
+import { VehicleService } from '../vehicle.service';
+import { Vehicle } from '../vehicle';
+import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-vehicle-detail',
-  templateUrl: './vehicle-detail.component.html',
-  styleUrls: ['./vehicle-detail.component.css']
+  selector: "app-vehicle-detail",
+  templateUrl: "./vehicle-detail.component.html",
+  styleUrls: ["./vehicle-detail.component.css"]
 })
 export class VehicleDetailComponent implements OnInit {
 
-  constructor() { }
+  vehicle: Vehicle;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private vehicleService: VehicleService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
+    this.getVehicle();
   }
 
+  getVehicle(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.vehicleService.getVehicle(id)
+      .subscribe(vehicle => this.vehicle = vehicle);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- HTL-Diplomprojekt-Angular/src/app/vehicle/vehicle-routing.module.ts
+++ HTL-Diplomprojekt-Angular/src/app/vehicle/vehicle-routing.module.ts
@@ -1,8 +1,14 @@
+import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
+import { VehicleLastScannedComponent } from './vehicle-last-scanned/vehicle-last-scanned.component';
+import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
+import { VehicleComponent } from './vehicle.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vehicle/:id', component: VehicleDetailComponent, data: { title: 'Vehicle Detail' } }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
