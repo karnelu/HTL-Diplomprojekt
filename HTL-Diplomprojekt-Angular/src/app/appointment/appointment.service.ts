35ceef84a2c4c0b01ea147cd8766f2e4d75df766
TheSteff5 <stefanmiskovic11@hotmail.com>
2019-12-12 10:42:41 +0100

    UI of newAppointment

--- HTL-Diplomprojekt-Angular/src/app/appointment/appointment.service.ts
+++ HTL-Diplomprojekt-Angular/src/app/appointment/appointment.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Appointment } from 'src/app/appointment/appointment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { connection } from 'src/app/connection';
-import { Observable } from 'rxjs';
+import { Observable, of} from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +19,21 @@ export class AppointmentService {
 
   constructor(private http: HttpClient) { }
 
-  newAppointment(appointment: Appointment): Observable<Appointment>{
-    return this.http.post<Appointment>(this.appointmentUrl + 'new', appointment, this.httpOptions);
+  newAppointment(appointment: Appointment): Observable<any>{
+    return this.http.post<Appointment>('http://localhost:8080/appointment/new', appointment, this.httpOptions).pipe(
+      catchError(this.handleError<Appointment[]>('new', [])));
   }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
 
 
