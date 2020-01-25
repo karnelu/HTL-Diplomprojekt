0596af181403ac8e165607b1480f45aaedfb8560
Lohni <andi.lohninger@hotmail.com>
2020-01-25 14:32:46 +0100

    Fixed time format

--- HTL_Diplomprojekt-Backend/src/main/java/com/porscheinformatik/htl/ICS_Generator.java
+++ HTL_Diplomprojekt-Backend/src/main/java/com/porscheinformatik/htl/ICS_Generator.java
@@ -20,18 +20,23 @@ public static byte[] write(Appointment appointment){
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
         String[] currdate = timeStamp.split("_");
 
+        String startdate = appointment.getStart_date().replace("-","");
+        String enddate = appointment.getEnd_date().replace("-","");
+        String starttime = appointment.getStart_time().replace(":","") +"00";
+        String endtime = appointment.getEnd_time().replace(":","")+"00";
+
         String builder = timeStamp + ".ics";
 
         bos.write("BEGIN:VCALENDAR\r\n".getBytes(StandardCharsets.UTF_8));
         bos.write("VERSION:2.0\r\n".getBytes(StandardCharsets.UTF_8));
         bos.write("PRODID:server@projectname.com\r\n".getBytes(StandardCharsets.UTF_8));
         bos.write("BEGIN:VEVENT\r\n".getBytes(StandardCharsets.UTF_8));
-        bos.write(("UID:"+appointment.getStart_date()+appointment.getStart_time()+"@projectname.com\r\n").getBytes(StandardCharsets.UTF_8));
+        bos.write(("UID:"+startdate+starttime+"@projectname.com\r\n").getBytes(StandardCharsets.UTF_8));
         bos.write(("LOCATION:"+appointment.getAction()+"\r\n").getBytes(StandardCharsets.UTF_8));
         bos.write(("SUMMARY:"+appointment.getTitle()+"\r\n").getBytes(StandardCharsets.UTF_8));
         bos.write(("DESCRIPTION:"+appointment.getDescription()+"\r\n").getBytes(StandardCharsets.UTF_8));
-        bos.write(("DTSTART:"+appointment.getStart_date()+"T"+appointment.getStart_time()+"Z\r\n").getBytes(StandardCharsets.UTF_8));
-        bos.write(("DTEND:"+appointment.getEnd_date()+"T"+appointment.getEnd_time()+"Z\r\n").getBytes(StandardCharsets.UTF_8));
+        bos.write(("DTSTART:"+startdate+"T"+starttime+"Z\r\n").getBytes(StandardCharsets.UTF_8));
+        bos.write(("DTEND:"+enddate+"T"+endtime+"Z\r\n").getBytes(StandardCharsets.UTF_8));
         bos.write(("DTSTAMP:"+currdate[0]+"T"+currdate[1]+"Z\r\n").getBytes(StandardCharsets.UTF_8));
         bos.write("END:VEVENT\r\n".getBytes(StandardCharsets.UTF_8));
         bos.write("END:VCALENDAR\r\n".getBytes(StandardCharsets.UTF_8));
